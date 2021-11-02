4531a5bfd45005c4c25b4e309f2276c6c22fdbda
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2021-11-02 02:59:08 +0000

    6-10

--- main.cpp
+++ main.cpp
@@ -1,22 +1,85 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-void changeMe(int);
+//Function prototypes
+void showMenu();
+void showFees(double, int);
 
-int main() {
+
+int main() 
+{
+  int choice;//Holds menu choice
+  int months;//Holds number of months
+
+  //constants for the menu choices
+  const int ADULT_CHOICE = 1,
+            CHILD_CHOICE = 2,
+            SENIOR_CHOICE = 3,
+            QUIT_CHOICE = 4;
+
+  //constants for membership rates
+
+  const double ADULT = 40.00,
+               CHILD = 20.00,
+               SENIOR = 15.00;
   
-  int number =12;
 
-  cout << "Number is " << number << endl;
+  //SET Up numberic output formatting
 
-  changeMe(number);
-  
-  cout << "Number is " << number << endl;
+cout << fixed << showpoint << setprecision(2);
 
+do
+{//display the menu and get the user's choice
+    showMenu();
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    //validate the menu selection
+    while(choice < ADULT_CHOICE || choice > QUIT_CHOICE)
+    {
+      cout << "Please enter a valid menu choice: ";
+      cin >> choice;
+    }
+
+
+  //if the user doesn't want to quit, proceed.
+
+  if (choice != QUIT_CHOICE)
+  {
+    // Get the number of months.
+    cout << "For how many months? ";
+    cin >> months;
+
+    // Display the membership feed.
+    switch(choice)
+    {
+      case ADULT_CHOICE:
+          showFees(ADULT, months);
+          break;
+      case CHILD_CHOICE:
+          showFees(CHILD, months);
+          break;
+      case SENIOR_CHOICE:
+          showFees(SENIOR, months);
+          break;
+    }
+  }
+} while (choice != QUIT_CHOICE);
   return 0;
 } 
 
-void changeMe(int myValue){
-  myValue = 0;
-  cout << "Now number is " << myValue << endl;
+void showMenu()
+{
+  cout << "MENU\n"
+      << "1. ADULT\n"
+      << "2. CHILD\n"
+      << "3. SENIOR\n"
+      << "4. QUIT_CHOICE\n";
+}
+
+void showFees(double memberRate, int months)
+{
+  cout << " The total charges are $"
+      << (memberRate * months) << endl;
 }
