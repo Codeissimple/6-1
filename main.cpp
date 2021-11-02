03bc7b06a0d9dd24cb065dbdc975a9c602c456cb
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2021-11-02 02:14:23 +0000

    6-6

--- main.cpp
+++ main.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
 using namespace std;
 
-//Function header void displayMessage()
-void first(){
-  cout << "I'm now inside the function first\n";
-}
-//Above constitutes function definition
-
-void second(){
-  cout << "I'm now inside the function second\n";
-}
-
-void third(){
-  cout << "I'm now inside the function third\n";
-}
+void displayValue(int);
 
 int main() {
-  
-  cout << "I'm starting in function main\n";
-  first();
-  second();
-  third();
-  cout << "Back in function main again". \n;
+  displayValue(5);
   
   return 0;
 } 
+
+void displayValue(int num){
+  cout << "The value is " << num;
+}
