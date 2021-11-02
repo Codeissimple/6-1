9a451344e0692f124d4cb2d965675ac1611d25c4
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2021-11-02 02:15:54 +0000

    6-7

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@ void displayValue(int);
 
 int main() {
   displayValue(5);
+  displayValue(15);
+  displayValue(56);
+  displayValue(45);
+  displayValue(995);
   
   return 0;
 } 
 
 void displayValue(int num){
-  cout << "The value is " << num;
+  cout << "The value is " << num << "\n";
 }
