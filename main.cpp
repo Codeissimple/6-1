94a9d69c1d5c8a92523dc51d42ab94ab1b3bd7d2
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2021-11-02 03:30:08 +0000

    6-11

--- main.cpp
+++ main.cpp
@@ -1,85 +1,30 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
-//Function prototypes
-void showMenu();
-void showFees(double, int);
+//function prototype
+void divide(double, double);
 
-
-int main() 
+int main()
 {
-  int choice;//Holds menu choice
-  int months;//Holds number of months
+  double num1, num2;
 
-  //constants for the menu choices
-  const int ADULT_CHOICE = 1,
-            CHILD_CHOICE = 2,
-            SENIOR_CHOICE = 3,
-            QUIT_CHOICE = 4;
-
-  //constants for membership rates
-
-  const double ADULT = 40.00,
-               CHILD = 20.00,
-               SENIOR = 15.00;
-  
-
-  //SET Up numberic output formatting
-
-cout << fixed << showpoint << setprecision(2);
-
-do
-{//display the menu and get the user's choice
-    showMenu();
-    cout << "Enter your choice: ";
-    cin >> choice;
-
-    //validate the menu selection
-    while(choice < ADULT_CHOICE || choice > QUIT_CHOICE)
-    {
-      cout << "Please enter a valid menu choice: ";
-      cin >> choice;
-    }
-
-
-  //if the user doesn't want to quit, proceed.
-
-  if (choice != QUIT_CHOICE)
-  {
-    // Get the number of months.
-    cout << "For how many months? ";
-    cin >> months;
-
-    // Display the membership feed.
-    switch(choice)
-    {
-      case ADULT_CHOICE:
-          showFees(ADULT, months);
-          break;
-      case CHILD_CHOICE:
-          showFees(CHILD, months);
-          break;
-      case SENIOR_CHOICE:
-          showFees(SENIOR, months);
-          break;
-    }
-  }
-} while (choice != QUIT_CHOICE);
+  cout << "Enter two numbers and I will divide the first\n";
+  cout << "number by the second number: ";
+  cin >> num1 >> num2;
+  divide(num1, num2);
   return 0;
-} 
 
-void showMenu()
-{
-  cout << "MENU\n"
-      << "1. ADULT\n"
-      << "2. CHILD\n"
-      << "3. SENIOR\n"
-      << "4. QUIT_CHOICE\n";
 }
 
-void showFees(double memberRate, int months)
+void divide(double arg1, double arg2)
 {
-  cout << " The total charges are $"
-      << (memberRate * months) << endl;
+  if (arg2 == 0.0){
+    cout << "Sorry, cannot divide by 0";
+    cout << "Enter two numbers and I will divide the first\n";
+  cout << "number by the second number: ";
+    cin >> arg1 >> arg2;
+  }
+  else{
+    cout << (arg1 / arg2);
+  }
 }
